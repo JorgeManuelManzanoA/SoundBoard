88744110239fa5d2d4f999d6a74c7e248c262c3b
jorgemanzano <jorge.manzano@tecsup.edu.pe>
2024-05-13 16:27:03 -0500

    Initial Commit

---
+++ ManzanoSoundBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ManzanoSoundBoard
+//
+//  Created by Jorge Manuel Manzano Añamuro on 13/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
